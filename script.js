2ca82a81c0d95084cd412eb7c8826b0921e3b3f3
paulchrisc <128294135+paulchrisc@users.noreply.github.com>
2023-03-29 02:49:18 -0700

    overlayclasses

--- script.js
+++ script.js
@@ -5,14 +5,18 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button Cliked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-
-btnCloseModal.addEventListener('click', function () {
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
+
+const openModal = function () {
+  console.log('Button Cliked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal); //do not call function, only when clicked...ommit "()"
+overlay.addEventListener('click', closeModal);
